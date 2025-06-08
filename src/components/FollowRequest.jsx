81cf436673eefcade5a9d1f801ff753d33324861
HanNajib <arroyhan27@gmail.com>
2025-06-08 14:22:09 +0700

    follow request

--- src/components/FollowRequest.jsx
+++ src/components/FollowRequest.jsx
@@ -4,15 +4,27 @@ import { Link, useSearchParams } from 'react-router-dom'
 
 function FollowRequest() {
     const username = localStorage.getItem('facegram-username')
+    const [loadingUsername, setLoadingUsername] = useState(null)
     const [followers, setFollowers] = useState([])
     const fetchFollower = async () => {
         const res = await ax.get(`users/${username}/followers`)
         setFollowers(res.data.followers.filter(follower => follower.is_requested))
     }
 
+    const handleConfirm = async (username) => {
+        try {
+            setLoadingUsername(username);
+            await ax.put(`users/${username}/accept`);
+            setLoadingUsername(null);
+            fetchFollower(); // refresh list
+        } catch (error) {
+            setLoadingUsername(null);
+        }
+    }
+
     useEffect(() => {
         fetchFollower();
-    }, [])
+    })
     return (
         <div className="request-follow mb-4">
             <h6 className="mb-3">Follow Requests</h6>
@@ -21,9 +33,13 @@ function FollowRequest() {
                     <div className="card mb-2" key={item.id}>
                         <div className="card-body d-flex align-items-center justify-content-between p-2">
                             <Link to={"/profile/" + item.username}>@{item.username}</Link>
-                            <a href="" className="btn btn-primary btn-sm">
-                                Confirm
-                            </a>
+                            <button 
+                                onClick={() => handleConfirm(item.username)} 
+                                className="btn btn-primary btn-sm"
+                                disabled={loadingUsername === item.username}
+                            >
+                                {loadingUsername === item.username ? 'Loading...' : 'Confirm'}
+                            </button>
                         </div>
                     </div>
                 )
